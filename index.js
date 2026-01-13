55c39dea0b71b2cba9bc0f9ab626690e67ad1479
Tazminur Rahman <m2air@m2s-MacBook-Air.local>
2026-01-13 19:18:21 +0600

    Update Link

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const port = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     'http://localhost:5173', // Vite dev server
-    'https://erp-dashboard12.netlify.app' // Netlify production
+    'https://erp-dashboard12.netlify.app',
+    'https://erp-dashboard-umber.vercel.app/' // Netlify production
   ],
   credentials: true
 }));
